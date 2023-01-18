1e730670eb9bd278c940d19349be73f2ddbb8097
김호성 <dsrla123@naver.com>
2023-01-18 17:38:34 +0900

    docs: 230118 hs

---
+++ hskim/begin-react/src/map.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Map() {
+    return 
+    <head>
+	  <meta charset="utf-8"/>
+	  <title>Kakao 지도 시작하기</title>
+    </head>
+        
+
+  }
+  
+  export default Map;
