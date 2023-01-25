009c362cd7de3bb456436d3a7bd7df3fd03d45b5
윤소희 <elvmeldj@naver.com>
2023-01-25 17:53:31 +0900

    feat: websocket 공부 예제 추가

---
+++ sohee/websocket/src/main/java/com/example/websocket/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.websocket.messagingstompwebsocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Greeting {
+
+    private String content;
+
+}
