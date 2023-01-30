426be8161e00bad5d5bedef2299b2f1b71d900f2
윤소희 <elvmeldj@naver.com>
2023-01-30 17:53:37 +0900

    feat: Redis 샘플 프로젝트 추가

---
+++ sohee/reids_ex/src/test/java/com/example/reids_ex/ReidsExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reids_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReidsExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
