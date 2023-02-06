e1f08acce6248ed4d20b6fdd8bc441ddf76d56c0
윤소희 <elvmeldj@naver.com>
2023-02-06 14:45:29 +0900

    feat: 문자 웹 발신 구현

---
+++ sohee/sms/src/main/java/com/example/sms/controller/SMSController.java
@@ -0,0 +1,56 @@
+package com.example.sms.controller;
+
+import com.example.sms.dto.MessageDTO;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@CrossOrigin("*")
+public class SMSController {
+
+    @Value("${API_KEY}")
+    private String apiKey;
+
+    @Value("${API_SECRET_KEY}")
+    private String apiSecretKey;
+
+    private DefaultMessageService messageService;
+
+    @PostConstruct
+    private void init(){
+        this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecretKey, "https://api.coolsms.co.kr");
+    }
+
+    @PostMapping("/send-one")
+    public SingleMessageSentResponse sendOne(@RequestBody MessageDTO messageDTO) {
+        Message message = new Message();
+        messageDTO.makeSentence();
+        // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
+        message.setFrom("01092039218");
+        message.setTo(messageDTO.getPhoneNumber());
+        message.setText(messageDTO.getMessage());
+
+        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+        System.out.println(response);
+
+        return response;
+    }
+
+}
